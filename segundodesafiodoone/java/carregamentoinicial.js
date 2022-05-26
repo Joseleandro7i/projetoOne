70fb82c4c207379bf4cb461c3c733d53805c0842
leandro <leandrojose124leandro@gmail.com>
2022-05-26 19:00:37 -0300

    nao sei

--- segundodesafiodoone/java/carregamentoinicial.js
+++ segundodesafiodoone/java/carregamentoinicial.js
@@ -3,26 +3,57 @@ botaoini.addEventListener("click", function(event){
     event.preventDefault();
 });
 
+  var criarbotaonovojogo = document.querySelector(".botaonovojogo");
+    criarbotaonovojogo.style.visibility = "hidden";
+    var criarbotaodesistir = document.querySelector(".botaodesitir");
+    criarbotaodesistir.style.visibility = "hidden";
+
 function iniciarosrecusosdojogo() {
- 	var tela = document.querySelector("canvas");
-        tela.classList.add("tamanho");
+ 	              
         var botaoini = document.querySelector(".botaoini");
         botaoini.style.display = "none";
         var botaoadi = document.querySelector(".botaoadi");
         botaoadi.style.display = "none";
 
-}
+        var criarbotaonovojogo = document.querySelector(".botaonovojogo");
+        criarbotaonovojogo.style.visibility = "visible";
+        var criarbotaodesistir = document.querySelector(".botaodesitir");
+        criarbotaodesistir.style.visibility = "visible";
+            
+        var tela = document.querySelector("canvas");
+        tela.classList.add("tamanho");    
+          tela.style.background="#46798b";         
+        var pincel = tela.getContext("2d");
+        pincel.fillStyle = "#341b8f";
+         pincel.fillRect(7, 143, 20, 1);
+         var iniciarosrecusosdojogo = colocarbotoesrestantes();
+         var iniciarosrecusosdojogo = desenharetrangulo();
+         var iniciarosrecusosdojogo = desenhatriangulo();
+         var iniciarosrecusosdojogo = desenhasegretrangulo();
+         var iniciarosrecusosdojogo = desenhadetalhes();
+ }
 
-function desenharcamposdojogo(x, y) {
-    var tela = document.querySelector('canvas');
-    var pincel = tela.getContext('2d');
+ function colocarbotoesrestantes() {
     
-    pincel.fillStyle = "blue";
-    pincel.beginPath();
-    pincel.moveTo(x, y);
-    pincel.fillRect();
+
+    criarbotaonovojogo.classList.add("botaonovojogo");
+    var criarp = document.querySelector(".novojogo");
+    criarp.textContent = "novo jogo";
+
+    var criarbotaodesistir = document.querySelector(".botaodesitir");
+    criarbotaodesistir.classList.add("botaodesistir");
+    var criarpdesistir = document.querySelector(".desistir");
+    criarpdesistir.textContent = "desistir";
+    
+ }
+            
+ 
 
 
-}
 
-desenharcamposdojogo(200, 100);
+
+
+
+
+
+
